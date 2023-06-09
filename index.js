e7e08a8f20b90600bf984baa06eb751d310e2038
ArnabNayan <arnabnayan5@gmail.com>
2023-06-09 22:13:45 +0600

    admin user created

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m5uzxbx.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,6 +30,11 @@ async function run() {
     // const instructorsClassCollection=client.db("sportsdb").collection("instructorclass");
 
     //users api
+    app.get('/users',async(req,res)=>{
+      const result=await usersCollection.find().toArray();
+      res.send(result)
+    })
+
     app.post('/users',async(req,res)=>{
       const user=req.body;
       console.log(user)
@@ -41,6 +46,19 @@ async function run() {
       const result=await usersCollection.insertOne(user);
       res.send(result);
     
+    })
+
+    app.patch('users/admin/:id',async(req,res)=>{
+      const id=req.params.id;
+      const filter={_id:new ObjectId(id)};
+      const updateDoc = {
+        $set:{
+          role:'admin'
+        },
+      };
+      const result=await usersCollection.updateOne(filter,updateDoc);
+      res.send(result);
+
     })
 
     app.get('/classes',async(req,res)=>{
