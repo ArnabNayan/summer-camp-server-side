19cba524b14f8060cffa8c36973b3c47b1556008
ArnabNayan <arnabnayan5@gmail.com>
2023-06-09 23:01:14 +0600

    jwt token created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const cors=require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port=process.env.PORT || 5000;
 
@@ -28,6 +29,12 @@ async function run() {
     const classesCollection=client.db("sportsdb").collection("classes");
     const instructorsCollection=client.db("sportsdb").collection("instructors");
     // const instructorsClassCollection=client.db("sportsdb").collection("instructorclass");
+
+    app.post('/jwt',(req,res)=>{
+      const user=req.body;
+      const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '48h' })
+      res.send({token})
+    })
 
     //users api
     app.get('/users',async(req,res)=>{
