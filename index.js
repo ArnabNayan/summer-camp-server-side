370eb8e80b86c37c9061613878ae9c30d2bd4968
ArnabNayan <arnabnayan5@gmail.com>
2023-06-14 18:07:09 +0600

    initally finalize

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const usersCollection = client.db("sportsdb").collection("users");
     const classesCollection = client.db("sportsdb").collection("classes");
     const instructorsCollection = client.db("sportsdb").collection("instructors");
@@ -282,7 +282,7 @@ app.patch('/instructorclass/:id/deny', async (req, res) => {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
